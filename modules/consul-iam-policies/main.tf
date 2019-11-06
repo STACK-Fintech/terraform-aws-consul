3f0e41f47a7ab9571efb01bc42ddc5ab6245e60f
Kevin Delisle <kjdelisle@gmail.com>
2019-11-06 15:51:58 -0500

    feat: add IAM rules for the server role

--- modules/consul-iam-policies/main.tf
+++ modules/consul-iam-policies/main.tf
@@ -2,12 +2,6 @@
 # ATTACH AN IAM POLICY THAT ALLOWS THE CONSUL NODES TO AUTOMATICALLY DISCOVER EACH OTHER AND FORM A CLUSTER
 # ---------------------------------------------------------------------------------------------------------------------
 
-resource "aws_iam_role_policy" "auto_discover_cluster" {
-  name   = "auto-discover-cluster"
-  role   = var.iam_role_id
-  policy = data.aws_iam_policy_document.auto_discover_cluster.json
-}
-
 data "aws_iam_policy_document" "auto_discover_cluster" {
   statement {
     effect = "Allow"
@@ -22,3 +16,61 @@ data "aws_iam_policy_document" "auto_discover_cluster" {
   }
 }
 
+data "aws_iam_policy_document" "dynamic_domain" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "route53:GetHostedZone",
+      "route53:ListHostedZonesByName",
+      "route53:ChangeResourceRecordSets",
+      "route53:ListResourceRecordSets",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy" "cloudwatch_agent" {
+  arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+data "aws_iam_policy" "ssm_agent" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMAutomationRole"
+}
+
+data "aws_iam_policy" "ssm_ec2" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+data "aws_iam_policy" "route53" {
+  arn = "arn:aws:iam::aws:policy/AmazonRoute53AutoNamingRegistrantAccess"
+}
+
+resource "aws_iam_role_policy" "auto_discover_cluster" {
+  name   = "auto-discover-cluster"
+  role   = var.iam_role_id
+  policy = data.aws_iam_policy_document.auto_discover_cluster.json
+}
+
+# Attach them to the AWS Instance Role
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+  role       = var.iam_role_id
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_agent" {
+  role       = var.iam_role_id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMAutomationRole"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_ec2" {
+  role       = var.iam_role_id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+resource "aws_iam_role_policy" "route53" {
+  name   = "AWSRoute53DynamicDomainPolicy"
+  role       = var.iam_role_id
+  policy = data.aws_iam_policy_document.dynamic_domain.json
+}
